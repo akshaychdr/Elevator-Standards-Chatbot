fb7320db218162835f9f475671a7799eb59e6cfe
akshaychdr <akshaychdr1991@gmail.com>
2023-04-04 12:50:49 +0530

    First Commit

---
+++ utils/makechain.ts
@@ -0,0 +1,60 @@
+import { OpenAIChat } from 'langchain/llms';
+import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
+import { PineconeStore } from 'langchain/vectorstores';
+import { PromptTemplate } from 'langchain/prompts';
+import { CallbackManager } from 'langchain/callbacks';
+
+const CONDENSE_PROMPT =
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`);
+
+const QA_PROMPT = PromptTemplate.fromTemplate(
+  `Instructions: Compose a comprehensive reply to the query using the search results given. 
+   You can cross-check information with your own search and can provide a better answer. 
+   Make sure the answer is correct and don't output false content. 
+   Ignore outlier search results which has nothing to do with the question.
+              
+Question: {question}
+=========
+{context}
+=========
+Answer in Markdown:`,
+);
+
+export const makeChain = (
+  vectorstore: PineconeStore,
+  onTokenStream?: (token: string) => void,
+) => {
+  const questionGenerator = new LLMChain({
+    llm: new OpenAIChat({ temperature: 0.1 }),
+    prompt: CONDENSE_PROMPT,
+  });
+  const docChain = loadQAChain(
+    new OpenAIChat({
+      temperature: 0.1,
+      modelName: 'gpt-3.5-turbo', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+      streaming: Boolean(onTokenStream),
+      callbackManager: onTokenStream
+        ? CallbackManager.fromHandlers({
+            async handleLLMNewToken(token) {
+              onTokenStream(token);
+              console.log(token);
+            },
+          })
+        : undefined,
+    }),
+    { prompt: QA_PROMPT },
+  );
+
+  return new ChatVectorDBQAChain({
+    vectorstore,
+    combineDocumentsChain: docChain,
+    questionGeneratorChain: questionGenerator,
+    returnSourceDocuments: true,
+    k: 1, //number of source documents to return
+  });
+};
